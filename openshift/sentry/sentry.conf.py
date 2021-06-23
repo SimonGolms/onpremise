59fdf3141c232c84bf5cb3e139f4e882d1409e10
Simon Golms <17799657+SimonGolms@users.noreply.github.com>
2021-06-23 14:55:06 +0200

    Update sentry.conf.py

--- openshift/sentry/sentry.conf.py
+++ openshift/sentry/sentry.conf.py
@@ -258,6 +258,8 @@ SENTRY_WEB_OPTIONS = {
     "thunder-lock": True,
     "log-x-forwarded-for": False,
     "buffer-size": 32768,
+    # Relay cannot authenticate without the following
+    "post-buffering": 32768,
     "limit-post": 209715200,
     "disable-logging": True,
     "reload-on-rss": 600,
